063ffa0a6909b20a0ebc6c63556342f0c22d9452
Masaya Takeda <844196@gmail.com>
2017-04-25 01:01:00 +0900

    add ncipher.Options constructor

--- lib/options.go
+++ lib/options.go
@@ -1,6 +1,7 @@
 package ncipher
 
 import (
+	"fmt"
 	"strconv"
 	"unicode/utf8"
 )
@@ -8,6 +9,52 @@ import (
 type Options struct {
 	Seed      string
 	Delimiter string
+}
+
+func NewOptions(seed, delimiter string) (*Options, error) {
+	opts := &Options{
+		Seed:      seed,
+		Delimiter: delimiter,
+	}
+
+	if err := opts.ensureValid(); err != nil {
+		return nil, err
+	}
+
+	return opts, nil
+}
+
+func (opts *Options) ensureValid() error {
+	if opts.seedSize() < 2 {
+		return fmt.Errorf("seed to short")
+	}
+
+	if opts.seedSize() > 36 {
+		return fmt.Errorf("seed to long")
+	}
+
+	encounter := make(map[rune]int, opts.seedSize())
+	for _, r := range opts.Seed {
+		encounter[r] += 1
+
+		if encounter[r] != 1 {
+			return fmt.Errorf("duplicate seed char: %s", r)
+		}
+	}
+
+	if utf8.RuneCountInString(opts.Delimiter) < 1 {
+		return fmt.Errorf("delimiter too short")
+	}
+
+	for _, dr := range opts.Delimiter {
+		for _, sr := range opts.Seed {
+			if dr == sr {
+				return fmt.Errorf("has include seed in delimiter: %s", dr)
+			}
+		}
+	}
+
+	return nil
 }
 
 func (opts *Options) seedSize() int {
--- lib/options_test.go
+++ lib/options_test.go
@@ -1,8 +1,50 @@
 package ncipher
 
 import (
+	"fmt"
 	"testing"
 )
+
+func TestNewOptions(t *testing.T) {
+	actual, err := NewOptions("こんにちは", "世界")
+
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	if fmt.Sprintf("%T", actual) != "*ncipher.Options" {
+		t.Errorf("expected: *ncipher.Options, actual:%T", actual)
+	}
+}
+
+func TestEnsureValid(t *testing.T) {
+	var o *Options
+
+	o = &Options{Seed: "世", Delimiter: "-"}
+	if err := o.ensureValid(); err == nil {
+		t.Errorf("expected error: seed to short")
+	}
+
+	o = &Options{Seed: "世世", Delimiter: "-"}
+	if err := o.ensureValid(); err == nil {
+		t.Errorf("expected error: duplicate seed char")
+	}
+
+	o = &Options{Seed: "世界", Delimiter: ""}
+	if err := o.ensureValid(); err == nil {
+		t.Errorf("expected error: delimiter too short")
+	}
+
+	o = &Options{Seed: "世界", Delimiter: "世"}
+	if err := o.ensureValid(); err == nil {
+		t.Errorf("expected error: has include seed in delimiter")
+	}
+
+	o = &Options{Seed: "こんにちは", Delimiter: "世界"}
+	if err := o.ensureValid(); err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+}
 
 func TestSeedSize(t *testing.T) {
 	opts := Options{
